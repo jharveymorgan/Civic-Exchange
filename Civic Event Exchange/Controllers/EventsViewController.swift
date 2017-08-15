30569c2bb3d76a4a2f07ac408835c420e3a3a8c2
jharveymorgan <jharveymorgan11@gmail.com>
2017-08-15 15:10:43 -0400

    Add files back and update the main storyboard

---
+++ Civic Event Exchange/Controllers/EventsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EventsViewController.swift
+//  Civic Event Exchange
+//
+//  Created by Jordan Harvey-Morgan on 8/15/17.
+//  Copyright © 2017 Jordan Harvey-Morgan. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("Events")
+    }
+
+}
---
+++ Civic Event Exchange/Controllers/TodayViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TodayViewController.swift
+//  Civic Event Exchange
+//
+//  Created by Jordan Harvey-Morgan on 8/15/17.
+//  Copyright © 2017 Jordan Harvey-Morgan. All rights reserved.
+//
+
+import UIKit
+
+class TodayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("Today")
+    }
+
+}
